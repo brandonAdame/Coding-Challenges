67e41a85d7a7ace95b134824a295ad3f0a6aa1c9
brandonAdame <brandonadame29@yahoo.com>
2017-07-17 19:19:14 -0400

    First Commit

---
+++ cmake-build-debug/CMakeFiles/Coding_Challenges.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Coding_Challenges.dir/main.cpp.o"
+  "Coding_Challenges.pdb"
+  "Coding_Challenges"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Coding_Challenges.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Coding_Challenges.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Coding_Challenges.
+# This may be replaced when dependencies are built.
